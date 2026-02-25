560cc9751ffd60ed9d121a1403a8b98d5872a3e5
payal-1804 <payalpandhare581@gmail.com>
2026-02-25 11:56:02 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,10 +1,11 @@
-public class HelloWorld {
-
-    // The main method is the entry point of the program
+public class DataTypesExample {
     public static void main(String[] args) {
-        // Print "Hello, World!" to the console
-        System.out.println("Hello, my self payal!");
+        int number = 10;
+        double price = 19.99;
+        boolean isTrue = true;
+        // Variables hold different data types
+        System.out.println("Number: " + number + ", Price: " + price);
     }
-
 }
+
 
